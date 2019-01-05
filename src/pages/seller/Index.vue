f66b520ce134c4f406edae5a275e3534320f79a3
Zane <push_over@163.com>
2019-01-05 18:26:52 +0800

    配置骨架

---
+++ src/pages/seller/Index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="goods">seller</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  linkActiveClass: 'active',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/goods',
+      name: 'goods',
+      component: () => import('../pages/goods/Index')
+    },
+    {
+      path: '/ratings',
+      name: 'ratings',
+      component: () => import('../pages/ratings/Index')
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: () => import('../pages/seller/Index')
     }
   ]
-})
+});
