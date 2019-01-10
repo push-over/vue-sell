fe9049a00bc1b02b3f69dc73f720b052511215e4
Zane <push_over@163.com>
2019-01-10 17:26:41 +0800

    组件拆分

--- src/pages/goods/Index.vue
+++ src/pages/goods/Index.vue
@@ -13,46 +13,27 @@
           </li>
         </ul>
       </div>
-      <div class="foods-wrapper" ref="foodsWrapper">
-        <ul>
-          <li v-for="(item,index) in goods" :key="index" class="food-list food-list-hook">
-            <h1 class="title">{{ item.name }}</h1>
-            <ul>
-              <li v-for="(food,index) in item.foods" :key="index" class="food-item border-1px">
-                <div class="icon">
-                  <img width="57" height="57" :src="food.icon" alt="">
-                </div>
-                <div class="content">
-                  <h2 class="name">{{ food.name }}</h2>
-                  <p class="desc">{{ food.description }}</p>
-                  <div class="extra">
-                    <span class="count">月售{{ food.sellCount }}份</span>
-                    <span>好评率{{ food.rating }}%</span>
-                  </div>
-                  <div class="price">
-                    <span class="now">￥{{ food.price }}</span>
-                    <span class="old" v-show="food.oldPrice">￥{{ food.oldPrice }}</span>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
+
+      <!-- 商品列表 -->
+      <v-item ref="foodsWrapper"></v-item>
+      <router-view></router-view>
     </div>
 </template>
 
 <script>
+import Item from './components/Item';
 import { mapState, mapActions } from 'vuex';
 import Bscroll from 'better-scroll';
+
 export default {
 
   created() {
-    let scroll = {
-      _initScroll: this._initScroll,
-      _calculateHeight: this._calculateHeight
-    };
-    this.getGoodsInfo(scroll);
+      let scroll = {
+        _initScroll: this._initScroll,
+        _calculateHeight: this._calculateHeight
+      };
+
+      this.getGoodsInfo(scroll);
   },
 
   computed: {
@@ -88,7 +69,7 @@ export default {
       this.meunScroll = new Bscroll(this.$refs.menuWrapper, {
         click: true
       });
-      this.foodsScroll = new Bscroll(this.$refs.foodsWrapper, {
+      this.foodsScroll = new Bscroll(this.$refs.foodsWrapper.$refs.foodsWrapper, {
         probeType: 3
       });
 
@@ -99,7 +80,7 @@ export default {
 
     // 计算高度
     _calculateHeight() {
-      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      let foodList = this.$refs.foodsWrapper.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
       let height = 0;
       this.changListHeight(height);
       for (let i = 0; i < foodList.length; i++) {
@@ -109,16 +90,20 @@ export default {
       }
     },
 
-    // 点击切换到对应的位置
+    // 点击自动切换到对应的位置
     selectMenu(index, event) {
       if (!event._constructed) {
         return;
       };
 
-      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      let foodList = this.$refs.foodsWrapper.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
       let el = foodList[index];
       this.foodsScroll.scrollToElement(el, 300);
     }
+  },
+
+  components: {
+    'v-item': Item
   }
 };
 </script>
@@ -174,54 +159,4 @@ export default {
           vertical-align middle
           // border-1px(rgba(7, 17, 27, 0.1))
           font-size 12px
-    .foods-wrapper
-      flex 1
-      .title
-        padding-left 14px
-        height 26px
-        line-height 26px
-        border-left 2px solid #d9dde1
-        font-size 12px
-        color rgb(147, 153, 159)
-        background #f3f5f7
-      .food-item
-        display flex
-        margin 18px
-        padding-bottom 18px
-        border-1px(rgba(7, 17, 27, 0.1))
-        &:last-child
-          border-none()
-          margin-bottom 0
-        .icon
-          flex 0 0 57px
-          margin-right 10px
-        .content
-          flex 1
-          .name
-            margin 2px 0 8px 0
-            height 14px
-            line-height 14px
-            font-size 14px
-            color rgb(7, 17, 27)
-          .desc, .extra
-            line-height 10px
-            font-size 10px
-            color rgb(147, 153, 159)
-          .desc
-            line-height 12px
-            margin-bottom 8px
-          .extra
-            .count
-              margin-right 12px
-          .price
-            font-weight 700
-            line-height 24px
-            .now
-              margin-right 8px
-              font-size 14px
-              color rgb(240, 20, 20)
-            .old
-              text-decoration line-through
-              font-size 10px
-              color rgb(147, 153, 159p)
 </style>
